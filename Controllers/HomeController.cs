00c934556de23b9eca667427fac05509c5d322e7
solomonov <d.solomonov@infocom-s.ru>
2021-02-11 20:34:25 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Lec2021.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lec2021.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TestDbConxextcs _TestDbConxextcs;
+
+        public HomeController(ILogger<HomeController> logger, TestDbConxextcs TestDbConxextcs)
+        {
+            _logger = logger;
+            _TestDbConxextcs = TestDbConxextcs;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData.Model =  _TestDbConxextcs.TestsModels.OrderBy(item => item.Id).ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TestDbConxextcs.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lec2021.Models
+{
+    public class TestDbConxextcs:DbContext
+    {
+        public DbSet<TestsModel> TestsModels { get; set; }
+        public TestDbConxextcs(DbContextOptions<TestDbConxextcs> dbContextOptions) : base(dbContextOptions)
+        {
+            Database.EnsureCreated();
+           if (!TestsModels.Any())
+            {
+                TestsModels.Add(new TestsModel
+                {
+                    Name = "Тест 1",
+                    Description = "ывпщмлыоващплыоавщ  ",
+                    isActive = true
+                });
+                TestsModels.Add(new TestsModel
+                {
+                    Name = "Тест 3",
+                    Description = "ывпщмлыоващплыоавщ  werwer",
+                    isActive = false
+                });
+                SaveChanges();
+            }
+        }
+    }
+}
